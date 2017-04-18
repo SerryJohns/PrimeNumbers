bc504567d92b12138fd4a138a2df72afb1d67d6d
SerryJohns <serembaj@gmail.com>
2017-04-18 12:54:42 +0300

    Generate prime numbers

--- prime.py
+++ prime.py
@@ -1,4 +1,22 @@
-def generate_prime_numbers(upper_bound):
+def generate_prime_numbers(n):
     prime_list = []
 
+    if (n > 1) and (n < 3):
+        return [2]
+    elif n < 2:
+        return []
+
+    odd_list = [x for x in range(3, n + 1) if x % 2 != 0]
+    odd_list = [2, 3] + [x for x in odd_list if x % 3 != 0]
+    count = 2
+
+    for my_number in odd_list:
+        for x in range(2, my_number + 1):
+            if my_number != x and my_number % x == 0:
+                break
+            elif my_number == x:
+                prime_list.append(my_number)
+
     return prime_list
+
+print(generate_prime_numbers(1))
